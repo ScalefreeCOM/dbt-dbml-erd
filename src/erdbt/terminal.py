50e1d6e19798ab0b4d8341b2e8f815d9e1b96d34
MahnazMirhaj <149672218+MahnazMirhaj@users.noreply.github.com>
2023-12-12 10:24:50 +0100

    Update terminal.py

--- src/erdbt/terminal.py
+++ src/erdbt/terminal.py
@@ -1,7 +1,6 @@
-import re 
 import click
 import subprocess
-from .core import genereatedbml
+from .core import genereatedbml,process_file
 @click.command()
 @click.argument('manifest_path', type=str)
 @click.argument('catalog_path', type=str)
@@ -36,36 +35,5 @@ def cli(manifest_path, catalog_path, erd_path, project_name, visualize, test_nam
             print(f"Error visualizing with dbdocs: {e}")
             
 
-def process_file(file_path):
-    """
-    Validates a DBML file and removes problematic lines by overwrriting the generated .dbml file.
-    Parameters:
-        file_path (str): The file path of the DBML file to be validated and cleaned.
-    Returns:
-        None
-    """
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    existing_tables = set()
-    for line in reversed(lines):
-        match = re.match(r'^Table \S+\.\S+ as (\S+)', line)
-        if match:
-            table_name = match.group(1)
-            existing_tables.add(table_name.strip())
 
-    for i, line in enumerate(lines):
-        if 'Ref:' in line:
-            referenced_table_match0 = re.search(r'Ref:\s*([^<>\s]+)', line)
-            referenced_table_match1 = re.search(r'(?<=>).*', line)
-            if referenced_table_match0 :
-                referenced_table_0 = referenced_table_match0.group(1).strip().split('.')[0]
-                if referenced_table_0 not in existing_tables:
-                    lines[i] = f'// {line}'
-            if referenced_table_match1:
-                referenced_table_1 = referenced_table_match1.group(0).strip().split('.')[0]
-                if referenced_table_1 not in existing_tables:
-                    lines[i] = f'// {line}'
-
-    with open(file_path, 'w') as file:
-        file.writelines(lines)
         
