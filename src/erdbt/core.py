40bd46d96a13f1083a4624af45b75eb1255d9e2c
MahnazMirhaj <149672218+MahnazMirhaj@users.noreply.github.com>
2023-12-12 10:24:28 +0100

    Update core.py

--- src/erdbt/core.py
+++ src/erdbt/core.py
@@ -1,6 +1,6 @@
 import json
 import logging
-
+import re 
 
 def loadModel(catalog_path, manifest_path):
     """Loads the dbt catalog and manifest files. The manifest selected is the one that will be used to generate the ERD diagram.
@@ -41,7 +41,7 @@ def createTable(dbml_path, model):
     columns = list(model["columns"].keys())
     start = "{"
     end = "}"
-
+  
     dbml_path.write(f"Table {schema}.{name} as {name} {start} \n")
 
     for column_name in columns:
@@ -110,12 +110,45 @@ def genereatedbml(manifest_path, catalog_path, dbml_path, test_name):
     """
     catalog, manifest = loadModel(catalog_path, manifest_path)
     model_names = sorted(list(catalog["nodes"].keys()))
-
     related_models = relatedModels(manifest, test_name)
 
     with open(dbml_path, "w") as dbml_file:
         for model_name in model_names:
             model = catalog["nodes"][model_name]
             if model["metadata"]["name"].lower() in related_models:
+          #     print(model["metadata"]["name"].lower(),'\n')
                 createTable(dbml_file, model)
         createRelatonship(dbml_file, manifest, test_name)
+        
+def process_file(file_path):
+    """
+    Validates a DBML file and removes problematic lines by overwrriting the generated .dbml file.
+    Parameters:
+        file_path (str): The file path of the DBML file to be validated and cleaned.
+    Returns:
+        None
+    """
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    existing_tables = set()
+    for line in reversed(lines):
+        match = re.match(r'^Table \S+\.\S+ as (\S+)', line)
+        if match:
+            table_name = match.group(1)
+            existing_tables.add(table_name.strip())
+
+    for i, line in enumerate(lines):
+        if 'Ref:' in line:
+            referenced_table_match0 = re.search(r'Ref:\s*([^<>\s]+)', line)
+            referenced_table_match1 = re.search(r'(?<=>).*', line)
+            if referenced_table_match0 :
+                referenced_table_0 = referenced_table_match0.group(1).strip().split('.')[0]
+                if referenced_table_0 not in existing_tables:
+                    lines[i] = f'// {line}'
+            if referenced_table_match1:
+                referenced_table_1 = referenced_table_match1.group(0).strip().split('.')[0]
+                if referenced_table_1 not in existing_tables:
+                    lines[i] = f'// {line}'
+
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
